8df1cb3a114cf43efa3ace937afdc064548e5114
bhargavjagtap <58814000+bhargavjagtap@users.noreply.github.com>
2022-02-01 08:45:18 +0530

    commit2:HeartIcon

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,10 +4,11 @@ import {
     SearchIcon,
     LibraryIcon,
     PlusCircleIcon,
-    HeartIcon,
     RssIcon,
-    LogoutIcon
 } from "@heroicons/react/outline"
+import {
+    HeartIcon,
+} from "@heroicons/react/solid"
 import { signOut, useSession } from 'next-auth/react';
 import useSpotify from '../hooks/useSpotify';
 import { useRecoilState } from 'recoil';
@@ -56,11 +57,11 @@ const Sidebar = () => {
                 <p className='ml-1'>Create Playlist</p>
             </button>
             <button className='flex items-center space-x-2 hover:text-white'>
-                <HeartIcon className="w-5 h-5" />
+                <HeartIcon className="w-5 h-5 text-blue-500" />
                 <p className='ml-1'>Liked Songs</p>
             </button>
             <button className='flex items-center space-x-2 hover:text-white'>
-                <RssIcon className="w-5 h-5" />
+                <RssIcon className="w-5 h-5 text-green-500" />
                 <p className='ml-1'>Your Episodes</p>
             </button>
             <hr className='border-t-[0.1px] border-gray-900' />
